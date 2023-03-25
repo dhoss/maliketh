59a490f027300ab79004630c67fcf34cc5e29c83
Devin Austin <devin.austin@gmail.com>
2023-03-25 15:46:35 -0600

    consolidate entry mapping

--- src/main/java/in/stonecolddev/maliketh/cms/api/entry/EntryResultSet.java
+++ src/main/java/in/stonecolddev/maliketh/cms/api/entry/EntryResultSet.java
@@ -5,9 +5,8 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.ZoneOffset;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntryResultSet implements ResultSetExtractor<List<Entry>> {
   @Override
@@ -15,26 +14,7 @@ public class EntryResultSet implements ResultSetExtractor<List<Entry>> {
     var entries = new ArrayList<Entry>();
 
     while (rs.next()) {
-      var entry = EntryBuilder.builder();
-      entry.id(rs.getInt("id"));
-      entry.body(rs.getString("body"));
-      entry.category(CategoryBuilder.builder().name(rs.getString("category_name")).build());
-      Timestamp created = rs.getTimestamp("created");
-      if (created != null) {
-        entry.created(created.toInstant().atOffset(ZoneOffset.UTC));
-      }
-      Timestamp updated = rs.getTimestamp("updated");
-      if (updated != null) {
-        entry.updated(updated.toInstant().atOffset(ZoneOffset.UTC));
-      }
-      entry.type(TypeBuilder.builder().name(rs.getString("entry_type")).build());
-      entry.published(rs.getBoolean("published"));
-      entry.slug(rs.getString("slug"));
-      entry.tags(new HashSet<String>(Arrays.asList((String[])rs.getArray("tags").getArray())));
-      entry.title(rs.getString("title"));
-      entry.author(UserBuilder.builder().userName(rs.getString("user_name")).build());
-      entry.version(rs.getInt("version"));
-      entries.add(entry.build());
+      entries.add(EntryMapper.fromResultSet(rs));
     }
 
     return entries;
