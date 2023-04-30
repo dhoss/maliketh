cef132663b18cf28b510e0b42603856f47237e69
Devin Austin <devin.austin@gmail.com>
2023-04-30 14:21:52 -0600

    added timezone configuration

--- src/main/java/in/stonecolddev/maliketh/cms/configuration/CmsConfiguration.java
+++ src/main/java/in/stonecolddev/maliketh/cms/configuration/CmsConfiguration.java
@@ -4,9 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Profile;
 
+import java.time.ZoneOffset;
+
 @ConfigurationProperties(prefix = "cms")
 @ConstructorBinding
 @Profile({"local", "unit-test", "it-test", "dev", "prod"})
 public record CmsConfiguration(
-  Integer pageSize
+  Integer pageSize,
+  ZoneOffset timeZone
 ) {}
